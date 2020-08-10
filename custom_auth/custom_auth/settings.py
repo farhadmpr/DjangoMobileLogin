09cc297caab1bb157f464b32ea95b7456af201de
farhadmpr <farhadmp@gmail.com>
2020-08-10 10:46:44 +0430

    Add custom MyUser, Manager

--- custom_auth/custom_auth/settings.py
+++ custom_auth/custom_auth/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'custom_login.apps.CustomLoginConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,16 +106,18 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'custom_login.MyUser'
--- custom_auth/custom_login/models.py
+++ custom_auth/custom_login/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from custom_login.my_user_manager import MyUserManager
 
-# Create your models here.
+
+class MyUser(AbstractUser):
+    username = None
+    mobile = models.CharField(max_length=11, unique=True)
+    otp = models.PositiveIntegerField(blank=True, null=True)
+    otp_create_time = models.DateTimeField(auto_now=True)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'mobile'
+
+    REQUIRED_FIELDS = []
+
+    backend = ''
